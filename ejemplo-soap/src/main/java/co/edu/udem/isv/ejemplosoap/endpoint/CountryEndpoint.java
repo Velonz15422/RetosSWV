7fdbd55fcc3691745363eb56ea40593a54b9cd93
Jose Lopez Velez <jilopez@udemedellin.edu.co>
2023-08-16 21:05:53 -0500

    - Se agrega el ejemplo de SOAP. - Se actualiza la colección de Postman para consumir la API. - Se actualiza el archivo Readme para incluir información importante para el curso

---
+++ ejemplo-soap/src/main/java/co/edu/udem/isv/ejemplosoap/endpoint/CountryEndpoint.java
@@ -0,0 +1,32 @@
+package co.edu.udem.isv.ejemplosoap.endpoint;
+
+import co.edu.udem.isv.ejemplo_soap.GetCountryRequest;
+import co.edu.udem.isv.ejemplo_soap.GetCountryResponse;
+import co.edu.udem.isv.ejemplosoap.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+
+@Endpoint
+public class CountryEndpoint {
+    private static final String NAMESPACE_URI = "http://udem.edu.co/isv/ejemplo-soap";
+
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+
+        return response;
+    }
+}
